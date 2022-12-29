c50fe276b985ee117bc70e9ea23d9a73fd8d30cc
熙 王 <1324771795@qq.com>
2022-12-29 22:38:10 +0800

    颜色转换

--- SilkWPF/Direct3D9/Sample/MiniTri.xaml.cs
+++ SilkWPF/Direct3D9/Sample/MiniTri.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using Silk.NET.Direct3D9;
 using Silk.NET.Maths;
+using System.Diagnostics;
 using System.Numerics;
 using System.Runtime.InteropServices;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace SilkWPF.Direct3D9.Sample;
 
@@ -18,11 +20,12 @@ public unsafe partial class MiniTri : UserControl
         public uint Color;
     }
 
+    private readonly Stopwatch _stopwatch = Stopwatch.StartNew();
     private readonly Vertex[] _vertices =
     {
-        new Vertex() { Color = 4294901760u, Position = new Vector4(400.0f, 100.0f, 0.5f, 1.0f) },
-        new Vertex() { Color = 4278190335u, Position = new Vector4(650.0f, 500.0f, 0.5f, 1.0f) },
-        new Vertex() { Color = 4278222848u, Position = new Vector4(150.0f, 500.0f, 0.5f, 1.0f) }
+        new Vertex() { Color = (uint)GameControl.ColorToBgra(Colors.Red), Position = new Vector4(400.0f, 100.0f, 0.5f, 1.0f) },
+        new Vertex() { Color = (uint)GameControl.ColorToBgra(Colors.Blue), Position = new Vector4(650.0f, 500.0f, 0.5f, 1.0f) },
+        new Vertex() { Color = (uint)GameControl.ColorToBgra(Colors.Green), Position = new Vector4(150.0f, 500.0f, 0.5f, 1.0f) }
     };
     private readonly Vertexelement9[] _vertexelements =
     {
@@ -31,8 +34,8 @@ public unsafe partial class MiniTri : UserControl
         new Vertexelement9(255, 0, 17, 0, 0, 0)
     };
 
-    private IDirect3DVertexBuffer9* vertices;
-    private IDirect3DVertexDeclaration9* ppDecl;
+    private IDirect3DVertexBuffer9* _ppVertexBuffer;
+    private IDirect3DVertexDeclaration9* _ppDecl;
 
     public MiniTri()
     {
@@ -50,23 +53,25 @@ public unsafe partial class MiniTri : UserControl
             fixed (Vertexelement9* vertexElems = &_vertexelements[0])
             {
                 void* ppbData;
-                Game.Device->CreateVertexBuffer(3 * 20, D3D9.UsageWriteonly, 0, Pool.Default, ref vertices, null);
-                vertices->Lock(0, 0, &ppbData, 0);
+                Game.Device->CreateVertexBuffer(3 * 20, D3D9.UsageWriteonly, 0, Pool.Default, ref _ppVertexBuffer, null);
+                _ppVertexBuffer->Lock(0, 0, &ppbData, 0);
                 System.Runtime.CompilerServices.Unsafe.CopyBlockUnaligned(ppbData, ptr, (uint)(sizeof(Vertex) * _vertices.Length));
-                vertices->Unlock();
+                _ppVertexBuffer->Unlock();
 
-                Game.Device->CreateVertexDeclaration(vertexElems, ref ppDecl);
+                Game.Device->CreateVertexDeclaration(vertexElems, ref _ppDecl);
             }
         }
     }
 
     private void Game_Render(TimeSpan obj)
     {
-        Game.Device->Clear(0, null, D3D9.ClearTarget, 4294937600u, 1.0f, 0);
+        float hue = (float)_stopwatch.Elapsed.TotalSeconds * 0.15f % 1;
+
+        Game.Device->Clear(0, null, D3D9.ClearTarget, (uint)GameControl.ColorToBgra(GameControl.HsvToColor(new Vector4(1.0f * hue, 1.0f * 0.75f, 1.0f * 0.75f, 1.0f))), 1.0f, 0);
         Game.Device->BeginScene();
 
-        Game.Device->SetStreamSource(0, vertices, 0, 20);
-        Game.Device->SetVertexDeclaration(ppDecl);
+        Game.Device->SetStreamSource(0, _ppVertexBuffer, 0, 20);
+        Game.Device->SetVertexDeclaration(_ppDecl);
         Game.Device->DrawPrimitive(Primitivetype.Trianglelist, 0, 1);
 
         Game.Device->EndScene();
